a46c16cb62722c2dc45a61f87dc0e60fb85bc3b7
heygus09 <carlos.ramirez6@catolica.edu.sv>
2024-11-12 22:13:47 -0600

    Cámara del scan ya inicia
    Se trabajo en la cámara la cual ya esta activa

---
+++ app/src/main/java/com/example/atenea/CaptureActivityPortrait.java
@@ -0,0 +1,8 @@
+package com.example.atenea;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureActivityPortrait extends CaptureActivity {
+    //QUEDA SOLO
+    //ES PARA LA PORTRAIT DE LA CAMARA
+}
--- app/src/main/java/com/example/atenea/home.java
+++ app/src/main/java/com/example/atenea/home.java
@@ -27,12 +27,12 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.fragment.app.ListFragment;
 
 import com.example.atenea.databinding.ActivityHomeBinding;
+import com.google.zxing.integration.android.IntentIntegrator;
 
 
 public class home extends AppCompatActivity {
 
     ActivityHomeBinding binding;
-
 
 
     @Override
@@ -45,6 +45,8 @@ public class home extends AppCompatActivity {
 
         replaceFragment(new HomeFragment());
         binding.bottomNavigationView.setBackground(null);
+
+
 
         binding.bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
 
@@ -61,6 +63,8 @@ public class home extends AppCompatActivity {
             return true;
 
         });
+
+
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -81,6 +85,7 @@ public class home extends AppCompatActivity {
     }
 
 
+
     private  void replaceFragment(Fragment fragment){
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -88,28 +93,59 @@ public class home extends AppCompatActivity {
         fragmentTransaction.commit();
     }
 
-    private void showBottomDialog(){
+    private void showBottomDialog()
+    {
 
-    final Dialog dialog = new Dialog(this);
-    dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-    dialog.setContentView(R.layout.fragment_scanfragment);
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.fragment_scanfragment);
 
-    ImageView cancelbutton = dialog.findViewById(R.id.cancel_button);
-    cancelbutton.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            dialog.dismiss();
-        }
-    });
+        ImageView cancelbutton = dialog.findViewById(R.id.cancel_button);
+            cancelbutton.setOnClickListener(new View.OnClickListener()
+            {
+            @Override
+            public void onClick(View v)
+            {
+                dialog.dismiss();
+            }
 
-    dialog.show();
-    dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
-    dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-    dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
-    dialog.getWindow().setGravity(Gravity.BOTTOM);
+
+        });
+
+        // Configurar botón de cámara para iniciar el escáner
+        ImageView cameraIcon = dialog.findViewById(R.id.camera_icon);
+        cameraIcon.setOnClickListener(v -> scanQRCode());
+
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
 
 
     }
+
+    private void scanQRCode()
+    {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+        integrator.setPrompt("Escanea el código QR");
+        integrator.setCameraId(0); // Cámara trasera
+        integrator.setOrientationLocked(false); // Forzar la orientacion vertical
+        integrator.setBeepEnabled(true); // Sonido al escanear
+        integrator.setCaptureActivity(CaptureActivityPortrait.class); //Solo se llama la clase la cual esta vacia
+        integrator.setBarcodeImageEnabled(false); // Guardar imagen OPCIONAL
+        integrator.initiateScan();
+    }
+
+
+
+
+
+
+
+
+
 
 
 
